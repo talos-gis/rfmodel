c1c35688ebd1f8e5f60053d68b3f42f57186c07b
Idan Miara <idan@miara.com>
2021-05-30 15:11:14 +0300

    build-rfmodel-wheel.bat - add twine upload command

--- build-rfmodel-wheel.bat
+++ build-rfmodel-wheel.bat
@@ -9,3 +9,8 @@ for /f %%i in ('dir /a:d /s /b src\*.egg-info') do rmdir /s/q %%i
 python -m twine check dist/*.whl
 
 if %1x==x call run_tests.bat
+
+:: uploading the dist via twine
+set twine_url=
+if %1x neq x set twine_url=--repository-url https://test.pypi.org/legacy/
+python -m twine upload %twine_url% dist/RFModel-*.whl
